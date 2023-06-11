edb4585c93eef81ae94906907fd6ed9f31f269da
charles <muchpaul2@gmail.com>
2023-06-11 23:15:31 +0300

    FIRST COMMIT

---
+++ app/src/main/java/com/judith/gbwa/AutofillCode.java
@@ -0,0 +1,14 @@
+package com.judith.gbwa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AutofillCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autofill_code);
+    }
+}
---
+++ app/src/main/java/com/judith/gbwa/TwoStepVerification.java
@@ -0,0 +1,14 @@
+package com.judith.gbwa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoStepVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_step_verification);
+    }
+}
